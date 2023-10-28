c464b95306d99ea73ae0ab0a248e0a133b2cec54
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-28 16:56:45 -0500

    Create function for handling errors

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const API_KEY = "98adb2a1ae6f44b6a4c183200232810";
 
+const displayData = (data) => {
+  const dataP = document.querySelector(".data > p");
+  dataP.textContent = data.location.name;
+};
+
+const displayError = (error) => {
+  const dataP = document.querySelector(".data > p");
+
+  if (error.message.includes("Parameter q")) {
+    error.message = "Please enter a city before searching.";
+  }
+
+  dataP.textContent = error.message;
+};
+
 const getWeatherData = async (query) => {
+  let data;
+  let response;
+
   try {
-    const response = await fetch(
+    response = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${query}&aqi=no`,
       { mode: "cors" }
     );
-    const data = await response.json();
-    console.log(data);
+    data = await response.json();
   } catch (error) {
-    console.log(error.message);
+    displayError(error);
   }
+
+  if (response.status > 299) {
+    return displayError(data.error);
+  }
+
+  return displayData(data);
 };
 
 const form = document.querySelector("form");
@@ -20,4 +43,5 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   getWeatherData(searchInput.value);
 });
-getWeatherData("london");
+
+getWeatherData();
