0707eccd458c81efda15f32b1e484f1dc3a5e993
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-28 16:29:42 -0500

    Add function for getting data

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test");
+const API_KEY = "98adb2a1ae6f44b6a4c183200232810";
+
+const getWeatherData = async (query) => {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${query}&aqi=no`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const form = document.querySelector("form");
+const searchInput = document.querySelector("input[type='search']");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherData(searchInput.value);
+});
+getWeatherData("london");
